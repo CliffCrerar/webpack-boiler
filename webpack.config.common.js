355b46c0357f9110614e309e9da4adad788acb08
CliffCrerar <cliff.crerar@gmail.com>
2019-11-17 15:35:54 +0200

    updated readme

--- webpack.config.common.js
+++ webpack.config.common.js
@@ -2,6 +2,7 @@ const path = require('path');
 const { CleanWebpackPlugin } = require('clean-webpack-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const ManifestPlugin = require('webpack-manifest-plugin');
+const webpack =require('webpack');
 
 const makePages = function(){
 	const {pages,meta,minify} = require('./src/pages');
